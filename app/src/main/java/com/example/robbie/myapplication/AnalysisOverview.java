253b572b85953702aa0b29e13d10cc2e5e81baec
Robbie Hamill <robbierabhamill@gmail.com>
2016-11-21 22:53:41 +0000

    Added Picture functionality

---
+++ app/src/main/java/com/example/robbie/myapplication/AnalysisOverview.java
@@ -0,0 +1,13 @@
+package com.example.robbie.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AnalysisOverview extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_analysis_overview);
+    }
+}
--- app/src/main/java/com/example/robbie/myapplication/CameraActivity.java
+++ app/src/main/java/com/example/robbie/myapplication/CameraActivity.java
@@ -2,22 +2,72 @@ package com.example.robbie.myapplication;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 import java.net.URI;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class CameraActivity extends Activity {
-    Button button;
+    private static final int CAMERA_REQUEST = 1888;
+    private ImageView imageView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+        this.imageView = (ImageView)this.findViewById(R.id.image_view);
+        Button photoButton = (Button) this.findViewById(R.id.button);
+        photoButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_REQUEST);
+            }
+        });
+
+        Button analysisButton = (Button) this.findViewById(R.id.button2);
+        photoButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_REQUEST);
+            }
+        });
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(photo);
+        }
+    }
+} /*  Button button;
     ImageView imageView;
+    String bob;
     static final int CAM_REQUEST = 1;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,31 +75,43 @@ public class CameraActivity extends Activity {
         setContentView(R.layout.activity_camera);
         button = (Button) findViewById(R.id.button);
         imageView = (ImageView) findViewById(R.id.image_view);
-        button.setOnClickListener(new View.OnClickListener(){
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                 File file = getFile();
                 camera_intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
-                startActivityForResult(camera_intent,CAM_REQUEST);
+                startActivityForResult(camera_intent, CAM_REQUEST);
 
             }
         });
+
     }
 
-    private File getFile(){
-        File folder = new File("My_Files/Images");
+    private File getFile() {
+      //  File folder = new File("storage/emulated/0/DCIM/camera_app");
+        String storageDir = Environment.getExternalStorageDirectory().getAbsolutePath();
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String fileName = timeStamp +"cam_image.jpg";
+        File folder = new File(storageDir+"/posture_App");
 
-        if(!folder.exists()){
+       // OutputStream output = new FileOutputStream(imageFile);
+        if (!folder.exists()) {
             folder.mkdir();
         }
-        File image_file = new File(folder,"cam_image.jpg");
+
+        File image_file = new File(folder,fileName );
+
+        bob = folder + fileName;
+        Log.d("MyAPP", "Iamhere");
         return image_file;
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        String path = "My_Files/Images/cam_image.jpg";
-        imageView.setImageDrawable(Drawable.createFromPath(path));
+        //String path = "storage/emulated/0/DCIM/camera_app/cam_image.jpg";
+        imageView.setImageDrawable(Drawable.createFromPath(bob));
     }
 }
+*/
+
