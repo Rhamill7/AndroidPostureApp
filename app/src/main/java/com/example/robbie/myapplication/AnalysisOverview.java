bdb0e90b37cd4e35ef43367e400552ad94ccc505
Robbie Hamill <robbierabhamill@gmail.com>
2016-11-22 15:09:03 +0000

    Adding in links between activities.

--- app/src/main/java/com/example/robbie/myapplication/AnalysisOverview.java
+++ app/src/main/java/com/example/robbie/myapplication/AnalysisOverview.java
@@ -1,7 +1,10 @@
 package com.example.robbie.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AnalysisOverview extends AppCompatActivity {
 
@@ -9,6 +12,16 @@ public class AnalysisOverview extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_analysis_overview);
+        Button nextButton =
+                (Button) this.findViewById(R.id.button3);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intentDetailed = new Intent(AnalysisOverview.this,DetailedAnalysis.class);
+                startActivity(intentDetailed);
+            }
+        });
 
     }
 }
